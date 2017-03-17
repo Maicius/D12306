870af7b51b4c414c73667866467a2c0c44273b17
DESKTOP-1VJF0IT\Maicius <maicius@outlook.com>
2017-03-17 21:23:02 +0800

    车站管理员增删查改完成

--- src/team/neu/ticket/controller/StationManagerController.java
+++ src/team/neu/ticket/controller/StationManagerController.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class StationManagerController {
     @Autowired
     ManagerService managerService;
-
+    StationInfo stationInfo;
 
 
     @RequestMapping(value="/manageStation")
@@ -39,7 +39,7 @@ public class StationManagerController {
         }catch(Exception e){
             station_id = 0;
         }
-        StationInfo stationInfo = new StationInfo(station_id, station_name, station_code,
+        stationInfo = new StationInfo(station_id, station_name, station_code,
                                                 railway_admin, station_class, station_region,
                                                 station_addr);
         List<StationInfo> stationInfos = managerService.queryStationInfo(stationInfo);
@@ -64,7 +64,7 @@ public class StationManagerController {
             station_id = 0;
         }
 
-        StationInfo stationInfo = new StationInfo(station_id, station_name, station_code,
+        stationInfo = new StationInfo(station_id, station_name, station_code,
                 railway_admin, station_class, station_region,
                 station_addr);
 
@@ -79,6 +79,51 @@ public class StationManagerController {
 
         ModelAndView mv = new ModelAndView();
 
+        mv.setViewName("station_manager_form");
+        return mv;
+    }
+
+    @RequestMapping(value="/deleteStation")
+    public ModelAndView deleteStation(@RequestParam(value="station_id") String station_id_s) throws Exception{
+        int station_id;
+        try{
+            station_id = Integer.parseInt(station_id_s);
+        }catch(Exception e){
+            station_id = 0;
+        }
+
+        int rs = managerService.deleteStationInfo(station_id);
+        List<StationInfo> stationInfos = managerService.queryStationInfo(stationInfo);
+        System.out.println(rs);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject(stationInfos);
+        mv.setViewName("station_manager_form");
+        return mv;
+    }
+
+    @RequestMapping(value="/addStation")
+    public ModelAndView addStation(@RequestParam(value="station_id") String station_id_S,
+                                   @RequestParam(value="station_name") String station_name,
+                                   @RequestParam(value="station_code") String station_code,
+                                   @RequestParam(value="railway_admin") String railway_admin,
+                                   @RequestParam(value="station_class") String station_class,
+                                   @RequestParam(value="station_region") String station_region,
+                                   @RequestParam(value="station_addr") String station_addr) throws Exception{
+        int station_id;
+        try{
+            station_id = Integer.parseInt(station_id_S);
+        }catch(Exception e){
+            station_id = 0;
+        }
+
+        stationInfo = new StationInfo(station_id, station_name, station_code,
+                railway_admin, station_class, station_region,
+                station_addr);
+        int rs = managerService.addStation(stationInfo);
+        List<StationInfo> stationInfos = managerService.queryStationInfo(stationInfo);
+        System.out.println(rs);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("stationInfos", stationInfos);
         mv.setViewName("station_manager_form");
         return mv;
     }
